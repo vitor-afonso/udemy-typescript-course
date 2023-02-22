e49bacf4d678d8ebab7d2cbc982a4674a2b04507
Vitor Afonso <vittokm@hotmail.com>
2023-02-22 13:38:35 +0000

    Initial commit =)

---
+++ 1_INTRO-aula-11/index.js
@@ -0,0 +1,9 @@
+var firstName = 'Casemiro';
+var anotherName = 1;
+var x = true;
+var greeting = function (name) {
+    console.log('Hello ' + name);
+};
+greeting(firstName);
+// greeting(anotherName);
+// greeting(x);
---
+++ 1_INTRO-aula-11/index.ts
@@ -0,0 +1,11 @@
+const firstName = 'Casemiro';
+const anotherName = 1;
+const x = true;
+
+const greeting = (name: string) => {
+  console.log('Hello ' + name);
+};
+
+greeting(firstName);
+// greeting(anotherName);
+// greeting(x);
