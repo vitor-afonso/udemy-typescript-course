f12f472389c5182d6613c1275db41948c6c767d2
Vitor Afonso <vittokm@hotmail.com>
2023-02-22 15:12:30 +0000

    solução desafio-1

--- 1_INTRO-aula-11/index.js
+++ 1_INTRO-aula-11/index.js
@@ -7,3 +7,12 @@ var greeting = function (name) {
 greeting(firstName);
 // greeting(anotherName);
 // greeting(x);
+/*********** DESAFIO 1 **********/
+// The challenge here is to create a function that (exclusively) takes 2 arguments of type number, adds them and returns the result
+var firstNum = 7;
+var secondNum = 21;
+var sumNumbers = function (num1, num2) {
+    return num1 + num2;
+};
+var result = sumNumbers(firstNum, secondNum);
+console.log({ result: result });
--- 1_INTRO-aula-11/index.ts
+++ 1_INTRO-aula-11/index.ts
@@ -9,3 +9,18 @@ const greeting = (name: string) => {
 greeting(firstName);
 // greeting(anotherName);
 // greeting(x);
+
+/*********** DESAFIO 1 **********/
+
+// The challenge here is to create a function that (exclusively) takes 2 arguments of type number, adds them and returns the result
+
+const firstNum = 7;
+const secondNum = 21;
+
+const sumNumbers = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
+
+const result = sumNumbers(firstNum, secondNum);
+
+console.log({ result });
