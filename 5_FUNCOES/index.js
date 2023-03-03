39a23b0ac67e461e288bc79f749476da66c90f23
Vitor Afonso <vittokm@hotmail.com>
2023-03-03 12:11:47 +0000

    completed functions section

--- 5_FUNCOES/index.js
+++ 5_FUNCOES/index.js
@@ -32,7 +32,7 @@ const mergeObjects = (obj1, obj2) => {
 };
 const newObject = mergeObjects({ name: 'Fernando' }, { age: 77, country: 'United States' });
 console.log(newObject);
-// 4 - constraints
+// 4 - generic with constraints
 const biggestNumber = (a, b) => {
     let biggest;
     if (+a > +b) {
@@ -46,3 +46,57 @@ const biggestNumber = (a, b) => {
 console.log(biggestNumber(3, 8));
 console.log(biggestNumber('5', '12'));
 //console.log(biggestNumber('12', 7));
+// 5 - especificar tipo de argumento
+const mergeArrays = (arr1, arr2) => {
+    return arr1.concat(arr2);
+};
+const newArr = mergeArrays([1, 2, 3], ['a', 'b']);
+console.log(newArr);
+// 6 - parametros opcionais
+function modernGreeting(name, greet) {
+    if (greet) {
+        return `Olá ${greet} ${name}, tudo bem?`;
+    }
+    return `Olá ${name}!`;
+}
+console.log(modernGreeting('Casemiro', 'Sr.'));
+console.log(modernGreeting('Fernando'));
+// 7 - parametro default
+function somaDefault(n, m = 10) {
+    return n + m;
+}
+console.log(somaDefault(5));
+console.log(somaDefault(5, 20));
+// 8 - unknown
+function doSomething(x) {
+    //console.log(x[0])
+    // unknown does not let us do more advance operations without narrowing it
+    if (Array.isArray(x)) {
+        console.log(x[0]);
+    }
+    else if (typeof x === 'number') {
+        console.log(`${x} é um numero.`);
+    }
+    else {
+        console.log('X não é numero nem é array.');
+    }
+}
+doSomething(8);
+doSomething([1, 2, 3]);
+// 9 - never
+function showErrorMsg(msg) {
+    throw new Error(msg);
+}
+// showErrorMsg('Algo deu errado!')
+// 10 - rest operator como parametro
+function sumAll(...n) {
+    return n.reduce((number, sum) => sum + number);
+}
+console.log(sumAll(1, 2, 3, 4));
+console.log(sumAll(4, 5, 6));
+// 11 - destructuring como parametro
+function showProductDetails({ name, price }) {
+    return `O nome do produto é ${name} e o seu preço é €${price}`;
+}
+const shirt = { name: 'Camisa', price: 59.99 };
+console.log(showProductDetails(shirt));
--- 5_FUNCOES/index.ts
+++ 5_FUNCOES/index.ts
@@ -61,3 +61,74 @@ const biggestNumber = <T extends string | number>(a: T, b: T): T => {
 console.log(biggestNumber(3, 8));
 console.log(biggestNumber('5', '12'));
 //console.log(biggestNumber('12', 7));
+
+// 5 - especificar tipo de argumento
+
+const mergeArrays = <T>(arr1: T[], arr2: T[]): T[] => {
+  return arr1.concat(arr2);
+};
+
+const newArr = mergeArrays<string | number>([1, 2, 3], ['a', 'b']);
+
+console.log(newArr);
+
+// 6 - parametros opcionais
+function modernGreeting(name: string, greet?: string) {
+  if (greet) {
+    return `Olá ${greet} ${name}, tudo bem?`;
+  }
+  return `Olá ${name}!`;
+}
+
+console.log(modernGreeting('Casemiro', 'Sr.'));
+console.log(modernGreeting('Fernando'));
+
+// 7 - parametro default
+function somaDefault(n: number, m = 10) {
+  return n + m;
+}
+
+console.log(somaDefault(5));
+console.log(somaDefault(5, 20));
+
+// 8 - unknown
+
+function doSomething(x: unknown) {
+  //console.log(x[0])
+  // unknown does not let us do more advance operations without narrowing it
+
+  if (Array.isArray(x)) {
+    console.log(x[0]);
+  } else if (typeof x === 'number') {
+    console.log(`${x} é um numero.`);
+  } else {
+    console.log('X não é numero nem é array.');
+  }
+}
+
+doSomething(8);
+doSomething([1, 2, 3]);
+
+// 9 - never
+function showErrorMsg(msg: string): never {
+  throw new Error(msg);
+}
+
+// showErrorMsg('Algo deu errado!')
+
+// 10 - rest operator como parametro
+function sumAll(...n: number[]) {
+  return n.reduce((number, sum) => sum + number);
+}
+
+console.log(sumAll(1, 2, 3, 4));
+console.log(sumAll(4, 5, 6));
+
+// 11 - destructuring como parametro
+function showProductDetails({ name, price }: { name: string; price: number }): string {
+  return `O nome do produto é ${name} e o seu preço é €${price}`;
+}
+
+const shirt = { name: 'Camisa', price: 59.99 };
+
+console.log(showProductDetails(shirt));
