84df828ee70ad9951b19fe0180b2fe7c2e2e9b9c
Vitor Afonso <vittokm@hotmail.com>
2023-03-28 18:22:09 +0100

    Add variables and function example

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,9 +1,21 @@
-import './App.css';
-
 function App() {
+  // 1 - variaveis
+  const name: string = 'Casemiro';
+  const age: number = 50;
+  const isWorking: boolean = true;
+
+  // 2 - funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}!`;
+  };
+
   return (
     <div className='App'>
-      <h1>TypeScript with react</h1>
+      <h1>React com TS</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && <p>Está trabalhando no momento</p>}
+      {userGreeting(name)}
     </div>
   );
 }
