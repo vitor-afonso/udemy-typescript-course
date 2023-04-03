eb2fdbaf81576971dc16e4d6797ca0b5542839d8
Vitor Afonso <vittokm@hotmail.com>
2023-04-03 11:38:52 +0100

    Create header and footer components

--- 12_todo_react/src/App.tsx
+++ 12_todo_react/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Todo Project</h1>
+      <Header />
+      <h1>Content...</h1>
+      <Footer />
     </div>
   );
 }
