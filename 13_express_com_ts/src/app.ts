5ca2713e3d011cb4c7fa7c3c58b31bc7c7f0da96
Vitor Afonso <vittokm@hotmail.com>
2023-04-12 12:51:39 +0100

    Start server and create first route

--- 13_express_com_ts/src/app.ts
+++ 13_express_com_ts/src/app.ts
@@ -1,3 +1,21 @@
 // 1 -  iniciando projecto
 
-console.log('Express + TS');
+//console.log('Express + TS');
+
+// 2 - init express
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', async (req, res, next) => {
+  try {
+    res.send('Hello from the backend!');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
+});
