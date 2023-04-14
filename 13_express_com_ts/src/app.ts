1e675f72da428c84cc97348a09ea6388694c96aa
Vitor Afonso <vittokm@hotmail.com>
2023-04-14 12:39:30 +0100

    Add JSON router params and middlewre

--- 13_express_com_ts/src/app.ts
+++ 13_express_com_ts/src/app.ts
@@ -4,7 +4,7 @@
 
 // 2 - init express
 
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 
@@ -36,12 +36,12 @@ app.all('/api/product/check', async (req, res) => {
   try {
     if (req.method === 'POST') {
       console.log('POST req.body =>', req.body);
-      return res.send('Product added.');
+      res.send('Product added.');
     } else if (req.method === 'GET') {
       console.log('GET request');
-      return res.send('Product retrieved.');
+      res.send('Product retrieved.');
     } else {
-      return res.send('Impossible to perform operation.');
+      res.send('Impossible to perform operation.');
     }
   } catch (error) {
     console.log(error);
@@ -53,6 +53,85 @@ app.all('/api/product/check', async (req, res) => {
 app.get('/api/interfaces', async (req: Request, res: Response) => {
   try {
     res.send('Using TypeScrip interfaces');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// 6 - enviando json
+
+app.get('/api/json', async (req: Request, res: Response) => {
+  try {
+    return res.json({
+      name: 'Shirt',
+      price: 19.9,
+      color: 'blue',
+      sizes: ['M', 'S', 'L'],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// 7 - router parameters
+
+app.get('/api/product/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = { id: '7', name: 'Some good stuff', price: 99 };
+    if (id === '7') {
+      res.status(200).json(product);
+    } else {
+      res.send('Product not found.');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// 8 - rotas complexas
+
+app.get('/api/product/:id/review/:reviewId', async (req: Request, res: Response) => {
+  try {
+    const { id, reviewId } = req.params;
+
+    res.send(`Getting review ${reviewId} from product ${id}.`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// 9 - router handler
+
+const getUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    res.send(`Getting user with id ${id}.`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+app.get('/api/user/:id', getUser);
+
+// 10 - middleware
+
+const checkUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  if (id === '10') {
+    console.log('Access granted');
+    next();
+  } else {
+    res.send('Access Denied');
+  }
+};
+
+app.get('/api/user/:id/access', checkUser, async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    res.json({ msg: 'Wellcome to the admin area.' });
   } catch (error) {
     console.log(error);
   }
