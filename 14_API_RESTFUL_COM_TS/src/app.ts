d3f5d252f5e0c132ae2b132b82be91d669fcc2b8
Vitor Afonso <vittokm@hotmail.com>
2023-04-19 10:03:27 +0100

    Set up router file and test route

--- 14_API_RESTFUL_COM_TS/src/app.ts
+++ 14_API_RESTFUL_COM_TS/src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import config from 'config';
+import router from './router';
 
 const app = express();
 
 // routes middleware
 app.use(express.json());
+
+// adds /api/ to the beginning of all routes from ./router
+app.use('/api/', router);
 
 const port = config.get<number>('port');
 
