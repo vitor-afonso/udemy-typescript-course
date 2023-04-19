8a028565d0880898280b98d79c44382ca9affbce
Vitor Afonso <vittokm@hotmail.com>
2023-04-19 09:45:32 +0100

    Set express

--- 14_API_RESTFUL_COM_TS/src/app.ts
+++ 14_API_RESTFUL_COM_TS/src/app.ts
@@ -1,1 +1,13 @@
-console.log('Funcionou!');
+import express from 'express';
+import config from 'config';
+
+const app = express();
+
+// routes middleware
+app.use(express.json());
+
+const port = config.get<number>('port');
+
+app.listen(3000, async () => {
+  console.log(`Listening on port: ${port}`);
+});
